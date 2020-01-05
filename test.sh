735c44ac0b6d7d6f8c8165afe873447ba0d6e209
Tadas <vtadas25@gmail.com>
2020-01-05 09:01:49 +0200

    Test search depth

--- test.sh
+++ test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-POS_BITS=8
+POS_BITS=12
 LENGTH_BITS=4
+SEARCH_DEPTH=2
 
 mkdir -p enc-files
 mkdir -p dec-files
@@ -11,8 +12,8 @@ archive="enc-files/$1.lzss"
 decoded="dec-files/$1"
 
 echo -n "Encoding..."
-# time RUST_BACKTRACE=1 RUST_LOG=debug cargo run -- encode $source_file $archive -c $LENGTH_BITS -s $POS_BITS -o > output.log 2>&1
-time cargo run -- encode $source_file $archive -c $LENGTH_BITS -s $POS_BITS -o
+time RUST_BACKTRACE=1 RUST_LOG=debug cargo run -- encode $source_file $archive -c $LENGTH_BITS -s $POS_BITS -o -d $SEARCH_DEPTH > output.log 2>&1
+# time cargo run -- encode $source_file $archive -c $LENGTH_BITS -s $POS_BITS -o -d $SEARCH_DEPTH
 echo -n "Decoding..."
 # time RUST_BACKTRACE=1 RUST_LOG=debug cargo run -- decode $archive $decoded -o > dec-output.log 2>&1
 time RUST_BACKTRACE=1 cargo run -- decode $archive $decoded -o 
